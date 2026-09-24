00ec023171ad25412b2a360f27d76472634c978e
Shrey Raj <shreyraj1653@gmail.com>
2026-09-24 13:27:24 +0530

    update: include only the production URL and localhost in CORS

--- server/app.js
+++ server/app.js
@@ -4,8 +4,12 @@ import { errorHandler } from "./src/middlewares/error.middleware.js";
 import healthCheckRouter from "./src/routes/healthcheck.route.js";
 import leadsRouter from "./src/routes/leads.route.js";
 
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  .split(",")
+  .map(origin => origin.trim());
+
 const app = express();
-app.use(cors("*"));
+app.use(cors({ origin: allowedOrigins }));
 app.use(express.json());
 
 app.use("/api/v1", healthCheckRouter);
